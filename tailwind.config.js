d41ac2db49ddf77cf46a710bfba92ce8aeb16e7a
anthrofax <afridhoikhsan@gmail.com>
2023-09-01 09:30:57 +0700

    Halaman awal 15%

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html, js}"],
+  content: ["./src/**/*.js", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black1': '#26292B',
+        'black2': '#2E3239',
+        'purple1': '#5F7ADB',
+        'purple1': '#A2B2EE',
+      },
+      fontFamily: {
+        'inria': "Inria",
+        'poppins':'Poppins'
+      }
+    },
   },
   plugins: [],
 }
