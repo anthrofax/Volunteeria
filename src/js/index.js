b1f798e7a944e76426ef2491f30062a1d2aaf0e2
anthrofax <afridhoikhsan@gmail.com>
2023-09-02 08:25:13 +0700

    Review Card Selesai

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,23 @@
 const cardRating = document.querySelectorAll('.card-rating');
+const dotSlider = document.querySelector('.dot-slider');
 
 cardRating.forEach((card, i) => {
-    console.log(i, card);
     card.style.transform = `translateX(${100 * i}%)`;
+
+    const circle = '<button class="w-3 h-3 bg-palewhite rounded-full"></button>';
+
+    dotSlider.insertAdjacentHTML('beforeend', circle);
+    // if (dotSlider.children[i].classList.contains('card-active')) dotSlider.children[i].classList.add('')
+    // console.log(dotSlider.children[i]);
+
+    const circleElement = [...dotSlider.children];
+
+    if (card.classList.contains('card-active')) {
+        circleElement[i].classList.add('bg-slate-100');
+        card.classList.remove('invisible');
+    } 
+
+    console.log(circleElement[i])
 })
+
+
