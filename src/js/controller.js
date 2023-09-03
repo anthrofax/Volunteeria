0986f638eb61bb98d79539e3f8d973de0f4af0cb
anthrofax <afridhoikhsan@gmail.com>
2023-09-03 19:07:06 +0700

    Animations

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,6 @@
 import carouselView from "./views/carouselView.js";
 import navbarView from "./views/navbarView.js";
+import contributionSection from "./views/contributionSection.js";
 
 const controlCarousel = function() {
     // Render Carousel & Dots
@@ -13,12 +14,21 @@ const controlCarousel = function() {
 }
 
 const controlNavbar = function() {
+    navbarView.navLinkHandler();
     navbarView.fixNavbar();
+}
+
+const controlContributionSection = function () {
+    contributionSection.init();
+    if (screen.width >= 1024) {
+        contributionSection.scrollAnimation();
+    }
 }
 
 const init = function() {
     controlCarousel();
     controlNavbar();
+    controlContributionSection();
 }
 
 init();
--- src/js/views/navbarView.js
+++ src/js/views/navbarView.js
@@ -1,24 +1,43 @@
 class navbarView {
+  _parentElement = document.querySelector("nav");
+  _navLinks = this._parentElement.querySelectorAll("a");
+
   fixNavbar() {
-    const nav = document.querySelector('nav');
+    const jumbotron = document.querySelector(".jumbotron");
+    const nav = this._parentElement;
 
-    const obsCallback = function (entries) {
-      const [entry] = entries;
-      console.log(entries);
-      if (entry.isIntersecting) nav.classList.replace('sticky-navbar', 'relative');
-      else nav.classList.replace('relative', 'sticky-navbar');
-      console.log(nav)
-    };
+    const navFixedObserver = new IntersectionObserver(
+      function (entries) {
+        const [entry] = entries;
+        if (entry.isIntersecting) nav.classList.replace("sticky-navbar", "relative");
+        else nav.classList.replace("relative", "sticky-navbar");
+      },
+      {
+        root: null,
+        threshold: 0,
+        rootMargin: `-${nav.getBoundingClientRect().height}px`,
+      }
+    );
 
-    const navFixedObserver = new IntersectionObserver(obsCallback, {
-      root: null,
-      threshold: 0,
-      rootMargin: `-${nav.getBoundingClientRect().height}px`,
-    });
-  
-    navFixedObserver.observe(document.querySelector('.jumbotron'));
+    navFixedObserver.observe(jumbotron);
   }
 
+  navLinkHandler() {
+    this._parentElement.addEventListener("mouseover", this.fadeOutLinks.bind(this,  .5));
+    this._parentElement.addEventListener("mouseout", this.fadeOutLinks.bind(this, 1));
+  }
+
+  fadeOutLinks(opacity, e) {
+    const linkHovered = e.target.closest("a");
+    
+    if (!linkHovered) return;
+
+    this._navLinks.forEach((link) => {
+      if (link !== linkHovered) {
+        link.style.opacity = opacity
+      }
+    });
+  }
 }
 
 export default new navbarView();
