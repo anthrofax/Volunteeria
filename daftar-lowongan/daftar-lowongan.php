4175b6eb11b4052cf4c282475d25ecc8ad09bb74
Zalzdarkent <you@example.com>
2023-09-21 17:29:04 +0700

    hapus kode yang tidak perlu

--- daftar-lowongan/daftar-lowongan.php
+++ daftar-lowongan/daftar-lowongan.php
@@ -113,29 +113,19 @@
   <!-- Mobile Sidebar -->
   <aside id="default-sidebar" class="xl:hidden side-navbar fixed top-0 left-0 z-50 w-64 h-screen transition-transform -translate-x-full" aria-label="Sidebar">
     <div class="h-full px-3 py-10 overflow-y-auto bg-gray-50 dark:bg-black1">
-      <ul class="space-y-2 font-medium divide-y-2 divide-black1 dark:divide-palewhite">
+      <ul class="space-y-2 font-medium">
         <li>
           <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-palewhite group">
-            <img src="../src/img/daftar-lowongan.svg" alt="" width="35px" class="dark:hidden" />
-            <img src="../src/img/daftar-lowongan-light.svg" alt="" width="35px" class="hidden dark:inline-block" />
+            <img src="../src/img/volunteer.svg" alt="" class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
             <span class="flex-1 ml-3 whitespace-nowrap text-purple1">Lowongan Volunteer</span>
           </a>
         </li>
         <li>
           <a href="../tips-volunteer/tips-volunteer.php" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-palewhite group">
-            <img src="../src/img/tips-volunteer2.svg" alt="" width="35px" class="dark:hidden" />
-            <img src="../src/img/tips-volunteer-light.svg" alt="" width="35px" class="hidden dark:inline-block" />
+            <img src="../src/img/tips.svg" alt="" class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
             <span class="flex-1 ml-3 whitespace-nowrap">Tips Volunteer</span>
           </a>
         </li>
-        <!-- <li>
-          <a href="" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-palewhite group">
-            <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-              <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z" />
-            </svg>
-            <span class="flex-1 ml-3 whitespace-nowrap">FOR EMPLOYERS</span>
-          </a>
-        </li> -->
       </ul>
     </div>
   </aside>
