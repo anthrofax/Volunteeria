dffc90f77523ee8e74eec4663be2cc7daee74e5f
Zalzdarkent <you@example.com>
2023-09-27 06:28:52 +0700

    validasi email

--- config/connect.php
+++ config/connect.php
@@ -7,7 +7,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['password'];
     $password_confirmation = $_POST['password_confirmation'];
     $remember_me = isset($_POST['remember_me']) ? 1 : 0; // Menggunakan 1 jika checkbox dicentang, 0 jika tidak
-    echo "Received password: " . $password_confirmation;
 
     // Validasi data yang diterima
     if (empty($name) || empty($email) || empty($password) || empty($password_confirmation)) {
@@ -19,17 +18,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     } elseif (!preg_match('/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/', $password)) {
         echo "Password harus mengandung setidaknya 3 huruf, 3 angka, 3 simbol, dan memiliki panjang minimal 8 karakter.";
     } else {
-        // Hash password sebelum menyimpannya
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        // Modifikasi SQL query untuk memeriksa apakah email sudah terdaftar sebelumnya
+        $check_email_sql = "SELECT * FROM users WHERE email = ?";
+        $check_email_stmt = $koneksi->prepare($check_email_sql);
+        $check_email_stmt->execute([$email]);
+        $existing_user = $check_email_stmt->fetch();
 
-        // Masukkan data ke dalam database
-        $sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
-        $stmt = $koneksi->prepare($sql);
-        if ($stmt->execute([$name, $email, $hashed_password])) {
-            // echo "Registrasi berhasil!";
-            header("Location: http://127.0.0.1:8000/admin/login");  // perlu redirect ke halaman html lagi
+        if ($existing_user) {
+            echo "Email sudah terdaftar. Silakan gunakan email lain.";
         } else {
-            echo "Terjadi kesalahan dalam proses registrasi.";
+            // Hash password sebelum menyimpannya
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+            // Masukkan data ke dalam database
+            $insert_sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+            $insert_stmt = $koneksi->prepare($insert_sql);
+            if ($insert_stmt->execute([$name, $email, $hashed_password])) {
+                // echo "Registrasi berhasil!";
+                header("Location: http://127.0.0.1:8000/admin/login");  // perlu redirect ke halaman html lagi
+            } else {
+                echo "Terjadi kesalahan dalam proses registrasi.";
+            }
         }
     }
 }
